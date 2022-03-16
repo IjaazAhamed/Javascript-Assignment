c2963cdb74b7c56b88d16f3a4171dd296bdb5961
IjaazAhamed <ijaazijju1713@gmail.com>
2022-03-16 20:56:39 +0530

    todo 8.2

--- assignments/todo_list/script.js
+++ assignments/todo_list/script.js
@@ -1,56 +1,70 @@
-async function details(){
-    let promise= await fetch('https://jsonplaceholder.typicode.com/todos')
-    let user=await promise.json()
-    console.log(user)
-    return user
+const compBtn = document.querySelector('button#comp');
+const pendBtn = document.querySelector('button#pend');
+const allbtn = document.querySelector('button#all');
+// const TodoInput = document.querySelector('input');
+// const addTodoBtn = document.querySelector('button');
+// const todolist = document.querySelector('.todos');
+// let count = 0;
+// addTodoBtn.onclick = function(){
+//     var input = TodoInput.value;
+//     if(input.length === 0) {
+//     alert("Please enter a task");
+//     } else {
+//     let p = document.createElement("p")
+//     p.setAttribute("key",count)
+//     p.innerHTML = input
+//     todolist.appendChild(p)
+//     document.querySelector(`p[key="${count}"]`).addEventListener('click',function(){
+//         todolist.removeChild(this)
+//     })
+//     count++;
+//     }
+// }
+async function fetchtodo(){
+    let res=await fetch('https://jsonplaceholder.typicode.com/todos');
+    console.log(res)
+    let data=await res.json();
+    console.log(data)
+    allbtn.onclick = function(){  
+        let output=''; 
+    data.forEach(function(todo,index) {
 
-}
-let data=details()
-output=''
-data.then(function(items){
-    for (let i=0;i<items.length;i++){
-        output+=`<p> <span key=${items[i].id}>${items[i].title} 
-        , id=${items[i].id}
-        ,completed=${items[i].completed}</span></p>`
-
+        output+=`<p key=${todo.id}>
+                "userId":  ${todo.userId},<br>
+                "id": ${todo.id},<br>
+                "title": ${todo.title},<br>
+                "completed":  ${todo.completed},<br>
+                </p>`
+})
+    document.querySelector('.todos').innerHTML=output;
     }
-    console.log(output)
-    let body=document.querySelector('#info')
-    body.innerHTML=output
-}
-)
-document.querySelector('#all').onclick=function(){ window.location.reload()}
-document.querySelector('#comp').onclick=function(){
-    output1=''
-    data.then(function(items){
-        for (let i=0;i<items.length;i++){
-            if (items[i].completed==true){
-            output1+=`<p> <span key=${items[i].id}>${items[i].title} 
-            , id=${items[i].id}
-            ,completed=${items[i].completed}</span></p>`}
+    compBtn.onclick = function(){  
+        let output=''; 
+    data.forEach(function(todo,index) {
+        if (todo.completed == true){
+        output+=`<p key=${todo.id}>
+                "userId":  ${todo.userId},<br>
+                "id": ${todo.id},<br>
+                "title": ${todo.title},<br>
+                "completed":  ${todo.completed},<br>
+                </p>`
+}})
+    document.querySelector('.todos').innerHTML=output;
+    }
+    pendBtn.onclick = function(){
+        let output='';
+        data.forEach(function(todo,index) {
+            if (todo.completed == false){
+            output+=`<p key=${todo.id}>
+                  "userId":  ${todo.userId},<br>
+                   "id": ${todo.id},<br>
+                   "title": ${todo.title},<br>
+                  "completed":  ${todo.completed},<br>
+                   
+                    </p>`
+    }})
 
+        document.querySelector('.todos').innerHTML=output;
         }
-        let body=document.querySelector('#info')
-        console.log(output1)
-        body.innerHTML=output1
-    }
-    )
 }
-
-document.querySelector('#pend').onclick=function(){
-    output1=''
-    data.then(function(items){
-        for (let i=0;i<items.length;i++){
-            if (items[i].completed==false){
-            output1+=`<p> <span key=${items[i].id}>${items[i].title} 
-            , id=${items[i].id}
-            ,completed=${items[i].completed}</span></p>`}
-
-        }
-        let body=document.querySelector('#info')
-        console.log(output1)
-        body.innerHTML=output1
-    }
-    )
-
-}
+    fetchtodo();
